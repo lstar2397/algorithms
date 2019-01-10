8bac0e7d84a1a5eda3c97e04b682fc5f61aaf214
Starlet <lstar2397@naver.com>
2019-01-10 19:58:19 +0900

    Programmers #12903: 가운데 글자 가져오기

---
+++ Programmers/src/12903/solution.cpp
@@ -0,0 +1,8 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+string solution(string s) {
+	int len = s.length();
+	return (len % 2) ? (s.substr(len / 2, 1)) : (s.substr(len / 2 - 1, 2));
+}
