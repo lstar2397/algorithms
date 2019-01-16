ca151a13dc754f1e6897645d4f68a24c0e188028
Starlet <lstar2397@naver.com>
2019-01-16 14:49:44 +0900

    Programmers #43165(cpp): 타겟 넘버

---
+++ Programmers/src/43165/solution.cpp
@@ -0,0 +1,23 @@
+#include <queue>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int dfs(queue<int> numbers, int target, int sum)
+{
+	if (numbers.size() == 0)
+		return (target == sum);
+
+	int front = numbers.front();
+	numbers.pop();
+
+	return dfs(numbers, target, sum + front) + dfs(numbers, target, sum - front);
+}
+
+int solution(vector<int> numbers, int target) {
+	queue<int> queue;
+	for (size_t i = 0; i < numbers.size(); i++)
+		queue.push(numbers.at(i));
+
+	return dfs(queue, target, 0);
+}
